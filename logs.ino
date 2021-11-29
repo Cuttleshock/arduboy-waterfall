c13f03bf08bacd9b5bb5bcd62b47c5d27718be58
Tori L <48884484+Cuttleshock@users.noreply.github.com>
2021-11-29 20:30:58 +0000

    Add top-level game state - TITLE or GAMEPLAY

--- logs.ino
+++ logs.ino
@@ -1,12 +1,24 @@
 #include <Arduboy2.h>
 
+#define TITLE 0
+#define GAMEPLAY 1
+
 Arduboy2 ab;
+int state;
+void setState(int state_)
+{
+  state = state_;
+}
+
 
 void setup()
 {
   ab.begin();
   ab.initRandomSeed();
   ab.setFrameRate(60);
+
+  setState(TITLE);
+
   ab.clear();
 }
 
@@ -17,6 +29,15 @@ void loop()
 
   ab.clear();
   ab.pollButtons();
+
+  switch (state)
+  {
+    case TITLE:
+      break;
+
+    case GAMEPLAY:
+      break;
+  }
 
   ab.display();
 }

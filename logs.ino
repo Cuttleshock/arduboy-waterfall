ae6e075d4f0d3816d2987f034ea86a22c4a168b0
Tori L <48884484+Cuttleshock@users.noreply.github.com>
2021-11-29 20:21:17 +0000

    Create Arduboy object and add boilerplate for it

--- logs.ino
+++ logs.ino
@@ -1,9 +1,22 @@
 #include <Arduboy2.h>
 
+Arduboy2 ab;
+
 void setup()
 {
+  ab.begin();
+  ab.initRandomSeed();
+  ab.setFrameRate(60);
+  ab.clear();
 }
 
 void loop()
 {
+  if (!ab.nextFrame())
+    return;
+
+  ab.clear();
+  ab.pollButtons();
+
+  ab.display();
 }
